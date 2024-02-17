e70769dd5e7478f57e3d2211c78e3c6069ebca32
MrBud <90530825+solocyberengineer@users.noreply.github.com>
2024-02-17 21:54:10 +0200

    Update pnp.js

--- pnp.js
+++ pnp.js
@@ -1,126 +1,80 @@
-let input = null;
-let searchBtn = null;
-let products = null;
-let nullTimeout = {
-  times: 4,
-  amt: []
+let nullTimeout = { times: 3, timeouts: 0 }
+let category = localStorage.getItem('category') ? JSON.parse(localStorage.getItem('category')) : { inCat: false, catIndex: 0, categories: [] };
+window._category = true;
+
+function waitRun(func){ window.interval = setInterval(()=>{func()}, 1000); }
+function timeout(url=null){ nullTimeout.timeouts += 1; if( nullTimeout.timeouts >= nullTimeout.times ) window.location.href = (url) ? url : 'https://www.pnp.co.za'; }
+function loadCategories(cat){
+  // console.log('loadCategories')
+  // console.log( category )
+  if( category.categories.length == 0 ){
+    // console.log('empty')
+    for( let _category of cat ) {
+      // console.log(_category.href);
+      category.categories.push(_category.href);
+    }
+    category.inCat = true
+    localStorage.setItem('category', JSON.stringify(category))
+  }
 }
-let HOST = window.location.host;
-
-let brand = ['Energade', 'toffo o luxe', 'beacon allsorts', 'expert kair', 'fattis ', 'aunt caroline', 'fizz pop', 'fizzer', 'beacon', 'benny', 'golden cloud', 'bio classic', 'halls', 'bio crystal', 'hugos', 'dolly varden', 'airoma', 'morvite', 'sunglish', 'ingrams', 'oros', 'jelly tots', 'peaceful sleep', 'jeyes', 'perfect touch', 'jungle', 'purity', 'king korn', 'koo', 'roses', 'smoothies', 'colmans', 'sparkeles', 'ace', 'cresta', 'maynards', 'spray and cook', 'crosses ', 'status', 'blackcat', 'albany', 'doom', 'no hair', 'tinkies', 'ice cap', 'all gold', 'mrs H.S.Balls', 'tastic', 'sunfoil', 'sunshine d', 'd lite', 'allsome', 'sona', 'crown', 'romi', 'woodenSpoon', 'Daily', 'sun gold', 'cordon blew', 'butterific', 'pastry king', 'kremolene', 'Greaso', 'Eve', 'Britelite', 'Waves', 'Crispa', 'Al Rajah', 'huggies', 'omo', 'domestos', 'five roses', 'bakers', 'simba', 'nescafe', 'nik naks', 'freshpak', 'Cremora', 'know', 'stork', 'nola', 'liqui fruit', 'kelloggs', 'McCain', 'bokomo', 'Snowflake', 'johnsons', 'white star', 'mortein', 'nestle', 'purity', 'ariel', 'dettol', 'cadbury', 'Hand Andy', 'Pampers', 'Robertsons', 'Spice Mecca', 'Mortein', 'Always', 'Black Cat', 'I&J', 'Hinds ', 'Parmalat', 'Countr Fresh', 'Rhodes', 'Dr Hahnz', 'House of Coffee', "Pieman's", 'Clover', 'Ace']
-let data = {}
-
-function index(){
-  let scraperInfo = localStorage.getItem('scraperInfo') ? JSON.parse( localStorage.getItem('scraperInfo') ) : {
-    index: 0
-  };
-  localStorage.setItem('scraperInfo', JSON.stringify(scraperInfo));
-  return scraperInfo.index;
+function getCategories(){
+  waitRun(()=>{
+    let categories = document.querySelector(`pnp-cms-facet` && `[tabindex='0']` && `[class='focus-lock ng-star-inserted']`);
+    if( categories ){
+      clearInterval( window.interval );
+      loadCategories( categories.querySelectorAll('a') );
+      if(category.categories.length > 0 && category.catIndex < category.categories.length){
+        let cat = document.querySelector(`[href="${category.categories[category.catIndex].split('https://www.pnp.co.za').at(-1)}"]`);
+        category.catIndex += 1;
+        window._category = false;
+        localStorage.setItem('category', JSON.stringify(category) );
+        cat.click();
+        console.log('clicked')
+        getProducts();
+        // window.location.href = 'http://www.pnp.co.za';
+      } else {
+        // perhaps iterate through the list here
+        localStorage.removeItem('category');
+        window.location.href = 'http://www.pnp.co.za';
+      }
+    }
+  })
 }
-
-function reload() {
-  window.location.href = 'https://www.pnp.co.za';
+function getProducts(timeoutUrl=null){
+  waitRun(()=>{
+    let products = document.querySelector('[data-cnstrc-search]');
+    if( products ){
+      clearInterval(window.interval);
+      // console.log('cat getProducts')
+      // console.log(_category)
+      if( window._category ){
+        getCategories();
+      } else {// else put it in a dict object 
+        console.log(products);
+        // window.location.href = 'https://www.pnp.co.za';
+      }
+    } else {
+      timeout(url=timeoutUrl);
+    }
+  });
+}
+function search(product_name){
+  waitRun(()=>{
+    let input = document.querySelector('[data-cnstrc-search-input]');
+    let searchBtn = document.querySelector('[data-cnstrc-search-submit-btn]');
+    if( input && searchBtn ){
+      clearInterval(window.interval);
+      input.value = product_name;
+      searchBtn.click();
+      // console.log('cat search')
+      // console.log(_category)
+      getProducts();
+    }
+  })
 }
 
 function main(){
-    let category = localStorage.getItem('category') ? JSON.parse(localStorage.getItem('category')) : {
-      inCategory: false,
-      categories: [],
-      catIter: 0
-    }
-    localStorage.setItem('category', JSON.stringify(category) );
-    if( category.inCategory ){
-      if( window.location.href != category.categories[category.catIter] ){
-        window.location.href = category.categories[category.catIter];
-      } else {
-        getProducts(category);
-      }
-    }
-    
-    function parseProducts(products){
-      console.log(products);
-      console.log('parsing products')
-    }
-    function parseCategories(categories){
-    }
-    function getProducts(category=null){
-      window.interval = setInterval(()=>{
-        products = document.querySelector('[data-cnstrc-search]');
-        if( products ){
-          clearInterval( window.interval );
-          // parseProducts( products.children ); // some how collect the products
-          if( category ){
-            if( category.catIter >= ( category.categories.length-1 ) ){
-              category.catIter = 0;
-              category.categories = [];
-              category.inCategory = false;
-              localStorage.setItem('category', JSON.stringify(category) );
-              let scraperInfo = JSON.parse( localStorage.getItem('scraperInfo') );
-              scraperInfo.index += 1;
-              localStorage.setItem("scraperInfo", JSON.stringify(scraperInfo));
-            } else {
-              category.catIter += 1;
-              category.inCategory = true;
-              localStorage.setItem('category', JSON.stringify(category))
-            }
-          } else {
-            let scraperInfo = JSON.parse( localStorage.getItem('scraperInfo') );
-            scraperInfo.index += 1;
-            localStorage.setItem("scraperInfo", JSON.stringify(scraperInfo));
-          }
-          nullTimeout.amt = [];
-          reload();
-        } else {
-          nullTimeout.amt.push(products);
-          if( nullTimeout.amt.length == nullTimeout.times ) reload();
-        }
-      }, 1000)
-    }
-    function enumerateCategory(){
-      window.interval = setInterval(()=>{
-        let categories = document.querySelector(`pnp-cms-facet` && `[tabindex='0']` && `[class='focus-lock ng-star-inserted']`);
-        
-        if( categories ){
-          clearInterval(window.interval);
-          let c = [];
-          for( let elem of categories.querySelectorAll('a') ){
-            let categoryUrl = `https://${HOST}${elem.getAttribute('href')}`;
-            c.push(categoryUrl);
-          }
-          category.inCategory = ( c.length > 0 );
-          category.categories = c;
-        } else {
-          console.log('here')
-          getProducts()
-        }
-        localStorage.setItem('category', JSON.stringify(category))
-      }, 1000);
-    }
-    function enumerateProducts(){
-      window.interval = setInterval(() => {
-        products = document.querySelector('[data-cnstrc-search]');
-        console.log( nullTimeout )
-        if( products ){
-          clearInterval(window.interval);
-          nullTimeout.amt = []
-          let productItems = products.children;
-          console.log(productItems);
-          enumerateCategory();
-        } else {
-          nullTimeout.amt.push(products)
-          if( nullTimeout.amt.length == nullTimeout.times ) reload();
-        }
-      }, 1000);
-    }
-    window.interval = setInterval(() => {
-        input = document.querySelector('[data-cnstrc-search-input]');
-        searchBtn = document.querySelector('[data-cnstrc-search-submit-btn]');
-        if( input && searchBtn ){
-          clearInterval(window.interval)
-          input.value = brand[ index() ];
-          searchBtn.click()
-          enumerateProducts();
-        }
-    }, 1000);
+  search('ultra mel');
 }
 main()
+
