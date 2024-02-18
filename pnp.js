1affb0edde333c19dc690a5283fecbf607caab88
MrBud <90530825+solocyberengineer@users.noreply.github.com>
2024-02-18 20:43:41 +0200

    Update pnp.js undergoing final testing

--- pnp.js
+++ pnp.js
@@ -1,7 +1,9 @@
-let nullTimeout = { times: 3, timeouts: 0 }
+let nullTimeout = { times: 3, timeouts: 0 };
 let category = localStorage.getItem('category') ? JSON.parse(localStorage.getItem('category')) : { inCat: false, catIndex: 0, categories: [] };
+let scraperInfo = localStorage.getItem('scraperInfo') ? JSON.parse(localStorage.getItem('scraperInfo')) : {index: 0, brands: []};
 window._category = true;
-
+window.isProducts = false;
+ 
 function waitRun(func){ window.interval = setInterval(()=>{func()}, 1000); }
 function timeout(url=null){ nullTimeout.timeouts += 1; if( nullTimeout.timeouts >= nullTimeout.times ) window.location.href = (url) ? url : 'https://www.pnp.co.za'; }
 function loadCategories(cat){
@@ -14,7 +16,7 @@ function loadCategories(cat){
       category.categories.push(_category.href);
     }
     category.inCat = true
-    localStorage.setItem('category', JSON.stringify(category))
+    localStorage.setItem('category', JSON.stringify(category) );
   }
 }
 function getCategories(){
@@ -24,6 +26,8 @@ function getCategories(){
       clearInterval( window.interval );
       loadCategories( categories.querySelectorAll('a') );
       if(category.categories.length > 0 && category.catIndex < category.categories.length){
+        console.log('cat')
+        console.log(category.categories[category.catIndex])
         let cat = document.querySelector(`[href="${category.categories[category.catIndex].split('https://www.pnp.co.za').at(-1)}"]`);
         category.catIndex += 1;
         window._category = false;
@@ -34,11 +38,22 @@ function getCategories(){
         // window.location.href = 'http://www.pnp.co.za';
       } else {
         // perhaps iterate through the brand list here and also set window._category to true
+        console.log('ayo')
         localStorage.removeItem('category');
-        // category.inCat = false;
+        scraperInfo.index += 1;
+        localStorage.setItem('scraperInfo', JSON.stringify(scraperInfo) );
+        window.location.href = 'http://www.pnp.co.za';
+      }
+    } else {
+      window._category = false;
+      if( window.products ){
+        clearInterval(window.interval);
+        scraperInfo.index += 1;
+        localStorage.setItem('scraperInfo', JSON.stringify(scraperInfo) );
         window.location.href = 'http://www.pnp.co.za';
       }
     }
+    console.log('running on repeat')
   })
 }
 function getProducts(timeoutUrl=null){
@@ -46,6 +61,7 @@ function getProducts(timeoutUrl=null){
     let products = document.querySelector('[data-cnstrc-search]');
     if( products ){
       clearInterval(window.interval);
+      window.products = true;
       // console.log('cat getProducts')
       // console.log(_category)
       if( window._category ){
@@ -77,7 +93,9 @@ function search(product_name){
 }
 
 function main(){
-  search('nestle');
+  if( scraperInfo.index < scraperInfo.brands.length ){
+    search(scraperInfo.brands[scraperInfo.index]);
+  }
 }
 main()
 
