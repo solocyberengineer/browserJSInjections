1f9a88ada1e1a871333e0e1cf4c6953f939cb416
MrBud <90530825+solocyberengineer@users.noreply.github.com>
2024-02-19 16:32:27 +0200

    Update pnp.js

--- pnp.js
+++ pnp.js
@@ -1,18 +1,37 @@
 let nullTimeout = { times: 3, timeouts: 0 };
 let category = localStorage.getItem('category') ? JSON.parse(localStorage.getItem('category')) : { inCat: false, catIndex: 0, categories: [] };
 let scraperInfo = localStorage.getItem('scraperInfo') ? JSON.parse(localStorage.getItem('scraperInfo')) : {index: 0, brands: []};
+window.data = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : {}; // { <brand>: {<categories>: [{<product-name>: <name>, <price>: <price>}]} }
 window._category = true;
 window.isProducts = false;
  
 function waitRun(func){ window.interval = setInterval(()=>{func()}, 1000); }
 function timeout(url=null){ nullTimeout.timeouts += 1; if( nullTimeout.timeouts >= nullTimeout.times ) window.location.href = (url) ? url : 'https://www.pnp.co.za'; }
+function setProducts(products){
+  let _category = window.location.href.split('http://www.pnp.co.za').at(-1).split(':').at(-1);
+  for(let product of products.children){
+    let productName = product.querySelector('[data-cnstrc-item-name]').getAttribute('data-cnstrc-item-name');
+    let productPrice = product.querySelector('[data-cnstrc-item-price]').getAttribute('data-cnstrc-item-price');
+    let productImage = product.querySelector('img').getAttribute('src');
+    let brandName = document.querySelector('[data-cnstrc-search-input]').value;
+    // data.push({brand: brandName});
+    if( !window.data[brandName] ) window.data[brandName] = {};
+    if( _category instanceof Array ){
+      if( !window.data[brandName]['no category'] ) window.data[brandName]['no category'] = [];
+      window.data[brandName]['no category'].push({productName, productPrice, productImage})
+    } else {
+      if( !window.data[brandName]['categories'] ) window.data[brandName]['categories'] = [];
+      if( !window.data[brandName]['categories'][_category] ) window.data[brandName]['categories'][_category] = [];
+      window.data[brandName]['categories'][_category].push({productName, productPrice, productImage});
+      console.log('categories')
+      console.log(_category)
+    }
+    localStorage.setItem('data', JSON.stringify(window.data));
+  }
+}
 function loadCategories(cat){
-  // console.log('loadCategories')
-  // console.log( category )
   if( category.categories.length == 0 ){
-    // console.log('empty')
     for( let _category of cat ) {
-      // console.log(_category.href);
       category.categories.push(_category.href);
     }
     category.inCat = true
@@ -26,19 +45,14 @@ function getCategories(){
       clearInterval( window.interval );
       loadCategories( categories.querySelectorAll('a') );
       if(category.categories.length > 0 && category.catIndex < category.categories.length){
-        console.log('cat')
-        console.log(category.categories[category.catIndex])
         let cat = document.querySelector(`[href="${category.categories[category.catIndex].split('https://www.pnp.co.za').at(-1)}"]`);
         category.catIndex += 1;
         window._category = false;
         cat.click();
-        console.log('clicked')
         getProducts();
         localStorage.setItem('category', JSON.stringify(category) );
-        // window.location.href = 'http://www.pnp.co.za';
       } else {
         // perhaps iterate through the brand list here and also set window._category to true
-        console.log('ayo')
         localStorage.removeItem('category');
         scraperInfo.index += 1;
         localStorage.setItem('scraperInfo', JSON.stringify(scraperInfo) );
@@ -46,14 +60,13 @@ function getCategories(){
       }
     } else {
       window._category = false;
-      if( window.products ){
+      if( window.isProducts ){
         clearInterval(window.interval);
         scraperInfo.index += 1;
         localStorage.setItem('scraperInfo', JSON.stringify(scraperInfo) );
         window.location.href = 'http://www.pnp.co.za';
       }
     }
-    console.log('running on repeat')
   })
 }
 function getProducts(timeoutUrl=null){
@@ -61,16 +74,15 @@ function getProducts(timeoutUrl=null){
     let products = document.querySelector('[data-cnstrc-search]');
     if( products ){
       clearInterval(window.interval);
-      window.products = true;
-      // console.log('cat getProducts')
-      // console.log(_category)
+      window.isProducts = true;
+      
       if( window._category ){
         getCategories();
-      } else {// else put it in a dict object 
-        console.log(products);
+      } else {
+        setProducts(products);
         setTimeout(()=>{
-          window.location.href = 'https://www.pnp.co.za';
-        }, 5000)
+          // window.location.href = 'https://www.pnp.co.za';
+        }, 1000)
       }
     } else {
       timeout(url=timeoutUrl);
@@ -85,8 +97,6 @@ function search(product_name){
       clearInterval(window.interval);
       input.value = product_name;
       searchBtn.click();
-      // console.log('cat search')
-      // console.log(_category)
       getProducts();
     }
   })
@@ -98,4 +108,3 @@ function main(){
   }
 }
 main()
-
